50afdb026a9894fa5156205a66d1a890b867c480
DannyMakito <72389518+DannyMakito@users.noreply.github.com>
2024-03-04 06:04:58 +0200

    Create login componet

---
+++ src/Components/LoginSignup/Login.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import './LoginSignup.css';
+import {FaLock,FaEnvelope} from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+
+//this is login page
+
+const Login =()=>{
+
+const[inputs,setInputs] = useState({})
+const handleChange = (event) =>{
+    const name = event.target.name;
+    const value = event.target.value;
+ setInputs(values =>({values,[name]: value})); ///stores all the details from form
+
+}
+const handleSubmit = (event) =>{
+    event.preventDefualt();
+    console.log(inputs);
+}
+    return(
+        <div className='container'>
+        <div className='header'>
+            <div className='text'>Login</div>
+            <div className='underline'></div>
+        </div>
+
+      <form onSubmit={handleSubmit}>
+      <div className='inputs'>
+            
+            <div className='input'>
+                <FaEnvelope className='icon'/>
+                <input type='email' placeholder='exapme@email.com' name="email" onChange={handleChange} />
+            </div>
+
+            <div className='input'>
+                <FaLock  className='icon'/>
+                <input type='password' placeholder='password' name="password" onChange={handleChange}/>
+            </div>
+
+        </div>
+         <div className="forgot-password">did you forget your password ?<span>click here</span></div>
+
+
+        <div className='submit-container'>
+            <div className="submitG">
+                <Link to='LoginSignup' className='l' >Sign Up</Link>
+            </div>
+            <button className="submit" >Login</button>
+        </div>
+      </form>
+    </div>
+
+    )
+
+
+
+}
+
+export default Login;
