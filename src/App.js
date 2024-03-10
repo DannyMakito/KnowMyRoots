e476fe8415a14dcf5728ecbcc7d6161879954a57
Makito <dmakito662@gmail.com>
2024-03-10 18:58:01 +0200

    created home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import LoginSignup from './Components/LoginSignup/LoginSignup';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Signup from "./Components/LoginSignup/Signup";
+import Login from "./Components/LoginSignup/Login";
+import Home from "./Components/Home";
 
 function App() {
   return (
-    <div >
-      <LoginSignup/>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
