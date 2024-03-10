36ed41b932727e9f7763687653cce862d8076cbd
Makito <dmakito662@gmail.com>
2024-03-10 19:04:57 +0200

    updated auth path of login

---
+++ src/Components/LoginSignup/Signup.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import "./LoginSignup.css";
+import { FaLock, FaUser, FaEnvelope } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const LoginSignup = () => {
+  const [inputs, setInputs] = useState({});
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value })); ///stores all the details from form
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+   // axios.post('http://localhost:23448/api/user/save', inputs);
+  };
+
+  return (
+    <div className="container">
+      <div className="header">
+        <div className="text">Sign up</div>
+        <div className="underline"></div>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="inputs">
+          <div className="input">
+            <FaUser className="icon" />
+            <input
+              type="text"
+              placeholder="name"
+              name="name"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="input">
+            <FaEnvelope className="icon" />
+            <input
+              type="email"
+              placeholder="example@email.com"
+              name="email"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="input">
+            <FaLock className="icon" />
+            <input
+              type="password"
+              placeholder="password"
+              name="password"
+              onChange={handleChange}
+            />
+          </div>
+
+          <div className="input">
+            <FaLock className="icon" />
+            <input
+              type="password"
+              placeholder="confirm password"
+              name="Cpassword"
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+
+        <div className="submit-container">
+          <button className="submit">Sign Up</button>
+
+          <div className="submitG">
+            <Link to="/login">Login</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginSignup;
